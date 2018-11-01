fd20f8c6456d3e6071a5c80404c733dcd2206958
Radu Maier <ariademur@webserv.co>
2018-11-01 12:26:38 +0100

    AbstractController set api config values

--- src/ParcelValue/ApiClient/AbstractController.php
+++ src/ParcelValue/ApiClient/AbstractController.php
@@ -3,6 +3,12 @@ namespace ParcelValue\ApiClient;
 
 abstract class AbstractController extends \WebServCo\Framework\AbstractController
 {
+    protected $apiUrl;
+    protected $apiVersion;
+    protected $clientId;
+    protected $clientKey;
+    protected $serverKey;
+
     protected $repository;
 
     use \ParcelValue\ApiClient\Traits\ControllerTrait;
@@ -22,5 +28,11 @@ abstract class AbstractController extends \WebServCo\Framework\AbstractControlle
                 $this->data('path/project')
             )
         );
+
+        $this->apiUrl = $this->config()->get('app/api/url');
+        $this->apiVersion = $this->config()->get('app/api/version');
+        $this->clientId = $this->config()->get('app/api/clientId');
+        $this->clientKey = $this->config()->get('app/api/clientKey');
+        $this->serverKey = $this->config()->get('app/api/serverKey');
     }
 }
