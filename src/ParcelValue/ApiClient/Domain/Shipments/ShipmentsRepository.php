f4d529e5715e87b44f1b1ac7c11b91dfef0344df
Radu Maier <ariademur@webserv.co>
2018-11-02 11:43:19 +0100

    use shipment object form configuration file

--- src/ParcelValue/ApiClient/Domain/Shipments/ShipmentsRepository.php
+++ src/ParcelValue/ApiClient/Domain/Shipments/ShipmentsRepository.php
@@ -2,83 +2,50 @@
 namespace ParcelValue\ApiClient\Domain\Shipments;
 
 use ParcelValue\Api\JsonApi\ResourceObjects\Shipment;
+use WebServCo\Framework\Exceptions\ApplicationException;
 
 final class ShipmentsRepository extends \ParcelValue\ApiClient\AbstractRepository
 {
-    public function getTestShipment()
+    public function getShipment()
     {
+        $shipmentConfig = $this->config()->load(
+            'Shipment',
+            $this->config()->get('path/project')
+        );
+        $this->verifyShipmentConfig($shipmentConfig);
+
         $shipment = new Shipment();
 
         $shipment->setAttribute('shipDate', date(Shipment::DATE_FORMAT, strtotime('tomorrow')));
 
-        $shipment->setAttribute(
-            'shipFrom',
-            [
-                'name' => 'Sender name',
-                'address1' => 'Sender street',
-                'city' => 'Milano',
-                'postalCode' => '20129',
-                'state' => 'MI',
-                'country' => 'IT',
-                'contact' => 'Sender contact name',
-                'phone' => '1234567890',
-                'email' => 'sender@ship.from',
-            ]
-        );
-
-        $shipment->setAttribute(
-            'shipTo',
-            [
-                'name' => 'Receiver name',
-                'address1' => 'Receiver street',
-                'city' => 'Muenchen',
-                'postalCode' => '80331',
-                'state' => null,
-                'country' => 'DE',
-                'contact' => 'Receiver contact name',
-                'phone' => '0987654321',
-                'email' => 'receiver@ship.to'
-            ]
-        );
-
-        $shipment->setAttribute(
-            'packages',
-            [
-                [
-                    'weight' => [
-                        'value' => '1.2',
-                        'units' => '1'
-                    ],
-                    'dimensions' => [
-                        'length' => '32',
-                        'width' => '33',
-                        'height' => '34',
-                        'units' => '1'
-                    ],
-                    'packageType' => 'CARTON',
-                ],
-                [
-                    'weight' => [
-                        'value' => '1.9',
-                        'units' => '1'
-                    ],
-                    'dimensions' => [
-                        'length' => '32',
-                        'width' => '33',
-                        'height' => '34',
-                        'units' => '1'
-                    ],
-                    'packageType' => 'CARTON',
-                ]
-            ]
-        );
-
-        $shipment->setAttribute('useCod', true);
-
-        $shipment->setAttribute('saturdayDelivery', true);
-
-        $shipment->setService('express');
+        $shipment->setAttribute('shipFrom', $shipmentConfig['attributes']['shipFrom']);
+        $shipment->setAttribute('shipTo', $shipmentConfig['attributes']['shipTo']);
+        $shipment->setAttribute('packages', $shipmentConfig['attributes']['packages']);
+        foreach (['useCod', 'saturdayDelivery'] as $item) {
+            if (isset($shipmentConfig['attributes'][$item])) {
+                $shipment->setAttribute($item, $shipmentConfig['attributes'][$item]);
+            }
+        }
+        $shipment->setService($shipmentConfig['meta']['service']);
 
         return $shipment;
+    }
+
+    protected function verifyShipmentConfig($shipmentConfig)
+    {
+        if (empty($shipmentConfig) || !is_array($shipmentConfig)) {
+            throw new ApplicationException('Missing or invalid shipment configuration');
+        }
+        foreach (['shipFrom', 'shipTo', 'packages'] as $item) {
+            if (!isset($shipmentConfig['attributes'][$item]) || !is_array($shipmentConfig['attributes'][$item])) {
+                throw new ApplicationException(
+                    sprintf('Missing or invalid shipment configuration attribute: %s', $item)
+                );
+            }
+        }
+        if (!isset($shipmentConfig['meta']['service'])) {
+            throw new ApplicationException('Missing or invalid shipment configuration meta: service');
+        }
+        return true;
     }
 }
