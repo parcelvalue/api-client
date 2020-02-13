c4370265b3fe6c6b32bbfcbdd7ddab544a9e980a
Radu Maier <ariademur@webserv.co>
2020-02-13 16:34:59 +0100

    validateAuthenticationToken

--- src/ParcelValue/ApiClient/Domain/Clients/Command.php
+++ src/ParcelValue/ApiClient/Domain/Clients/Command.php
@@ -1,6 +1,8 @@
 <?php
 namespace ParcelValue\ApiClient\Domain\Clients;
 
+use ParcelValue\Api\AuthenticationToken;
+
 use WebServCo\Framework\Cli\Ansi;
 use WebServCo\Framework\Cli\Sgr;
 
@@ -23,11 +25,37 @@ final class Command extends \ParcelValue\ApiClient\AbstractController
         $this->outputCli(Ansi::sgr(__METHOD__, [Sgr::BOLD]), true);
         $this->outputCli();
 
-        $jwt = \ParcelValue\Api\AuthenticationToken::generate($this->clientId, $this->clientKey, $this->serverKey);
+        $jwt = AuthenticationToken::generate($this->clientId, $this->clientKey, $this->serverKey);
         $this->outputCli(Ansi::sgr('Success!', [Sgr::GREEN]), false);
         $this->outputCli(' Your token is:', true);
         $this->outputCli();
         $this->outputCli($jwt, true);
+        $this->outputCli();
+        return new \WebServCo\Framework\Cli\Response('', true);
+    }
+
+    public function validateAuthenticationToken($token)
+    {
+        $this->outputCli(Ansi::clear(), true);
+        $this->outputCli(Ansi::sgr(__METHOD__, [Sgr::BOLD]), true);
+        $this->outputCli();
+        $this->outputCli(sprintf('Input: %s', $token), true);
+        $this->outputCli();
+
+        try {
+            $result = AuthenticationToken::decode($token, $this->serverKey);
+            $this->outputCli(Ansi::sgr('Success!', [Sgr::GREEN]), true);
+            $this->outputCli(var_export($result, true), true);
+        } catch (\Exception $e) {
+            $this->outputCli(
+                Ansi::sgr(
+                    sprintf('Error: %s', $e->getMessage()),
+                    [Sgr::RED]
+                ),
+                true
+            );
+        }
+
         $this->outputCli();
         return new \WebServCo\Framework\Cli\Response('', true);
     }
